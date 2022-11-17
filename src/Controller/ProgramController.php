403c91252b5c79df3f8646c2ebff7751e87c414f
Hman-dev <hermannflori@gmail.com>
2022-11-17 13:27:01 +0100

    05-symfony le routing avancé

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,14 +2,16 @@
 
 namespace App\Controller;
 
+use phpDocumentor\Reflection\Location;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 {
     
-    #[Route('/program/', name: 'program_index')]
+    #[Route('/', name:'index')]
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
@@ -18,4 +20,21 @@ class ProgramController extends AbstractController
      
          ]);
     }
+
+    #[Route('/{id<\d+>}', methods: ['GET'], name: 'id')]
+    public function show(int $id): Response
+    {
+        if ($id != (int)$id) 
+        {
+           
+            return $this->redirectToRoute("HTTP/1.1 404 Not Found");
+        }
+        
+    return $this->render('program/show.html.twig', [
+        
+            'list' => $id,
+     
+         ]);
+    }
+
 }
